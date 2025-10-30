04ae4132c7dd01c4e4becbc74cbe4b1beb3844b7
maxurban11-droid <max.urban.11@gmail.com>
2025-10-30 22:51:13 +0100

    Delete helpers directory

--- helpers/headers.ts
+++
@@ -1,9 +0,0 @@
-export function json(data: any, init: ResponseInit = {}) {
-  return new Response(JSON.stringify(data), {
-    ...init,
-    headers: {
-      "content-type": "application/json; charset=utf-8",
-      ...(init.headers || {})
-    }
-  });
-}
