300fbd3d0ca2aa4923fdef032c72b048d7c4b4eb
maxurban11-droid <max.urban.11@gmail.com>
2025-10-31 00:26:10 +0100

    Delete helpers directory

--- helpers/env.ts
+++
@@ -1,7 +0,0 @@
-export const isProd = process.env.NODE_ENV === "production";
-export const ORIGIN_WHITELIST = (process.env.ORIGIN_WHITELIST || "")
-  .split(",")
-  .map((s) => s.trim())
-  .filter(Boolean);
-export const SESSION_TTL_SECONDS = Number(process.env.SESSION_TTL_SECONDS || 60 * 60 * 24 * 7);
-export const NONCE_TTL_SECONDS = Number(process.env.NONCE_TTL_SECONDS || 10 * 60);
