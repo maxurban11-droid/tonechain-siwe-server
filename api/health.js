5fd1454bb5dba6341a3c6286d1f2d7b31d829ed3
maxurban11-droid <max.urban.11@gmail.com>
2025-11-01 00:33:33 +0100

    Update health.js

--- api/health.js
+++ api/health.js
@@ -4,5 +4,5 @@ const { withCors, handleOptions } = require("../helpers/cors.js");
 module.exports = withCors(async function handler(req, res) {
   if (req.method === "OPTIONS") return handleOptions(req, res);
   res.setHeader("Content-Type", "application/json; charset=utf-8");
-  res.status(200).send(JSON.stringify({ ok: true, health: "up" }));
+  res.status(200).json({ ok: true, health: "up" });
 });
