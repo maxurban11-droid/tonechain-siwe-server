9423aca24d7294e319bfd7a1ca160f011bea8b2c
maxurban11-droid <max.urban.11@gmail.com>
2025-11-09 13:14:08 +0100

    Delete api/health.js

--- api/health.js
+++
@@ -1,8 +0,0 @@
-// api/health.js
-const { withCors, handleOptions } = require("../helpers/cors.js");
-
-module.exports = withCors(async function handler(req, res) {
-  if (req.method === "OPTIONS") return handleOptions(req, res);
-  res.setHeader("Content-Type", "application/json; charset=utf-8");
-  res.status(200).json({ ok: true, health: "up" });
-});
